8290b655680a03333d714c2d47b1b0e6d32964b0
Swayam <swayamg741@gmail.com>
2025-07-26 22:46:41 +0530

    x-follow

---
+++ twitter_follow.py
@@ -0,0 +1,55 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+import time
+import traceback
+
+options = Options()
+options.add_experimental_option("debuggerAddress", "localhost:9222")
+
+driver = webdriver.Chrome(options=options)
+
+
+driver.get("https://x.com/home")
+try:
+
+    element = WebDriverWait(driver, 60).until(
+        EC.presence_of_element_located((By.XPATH, "//div[@data-testid='cellInnerDiv']"))
+    )
+
+    caret = element.find_element(By.XPATH, ".//button[@data-testid='caret']")
+    print("Page is ready!", caret)
+    caret.click()
+    time.sleep(5)
+
+
+    dropdown = WebDriverWait(driver, 20).until(
+        lambda e: e.find_element(By.XPATH, ".//div[@data-testid='Dropdown']")
+    )
+
+    print("Follow button found and clicked:", dropdown.text)
+    time.sleep(5)
+
+    follow_button = dropdown.find_element(
+        By.XPATH, ".//div[@role='menuitem'][contains(., 'Follow') and not(contains(., 'Following'))]"
+    )
+
+    print("Follow button found:", follow_button.text)
+    follow_button.click()
+
+except Exception as e:
+    print("An error occurred:", e)
+    traceback.print_exc()
+
+finally:
+    driver.quit()
+
+
+
+
+
+# "C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222 --user-data-dir="C:\Personal-space\eternity\chrome-profile"
+
